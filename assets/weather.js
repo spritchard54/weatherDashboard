92d57a29dc57e3279ea030e4ae09ec8a845fd880
spritchard54 <>
2023-11-12 15:24:51 -0500

    updated js, dynamically inserted tiles, fetch working

--- assets/weather.js
+++ assets/weather.js
@@ -1,101 +1,115 @@
-
-var searchBtn = $('#searchBtn');
-var requestUrl = 'https://api.openweathermap.org/data/3.0/onecall'
-var appKey = '?appid=a484d3fb0652e567788e79a914828975'//first parameter
-var lat = '&lat='
-var lon = '&lon='
-var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct'
-var cityGeo = '&q='
-
 console.log('Weather.js is connected');
 
-function firstFetch(userCity){
-    console.log(geoUrl + appKey + cityGeo + userCity);
-    fetch(geoUrl + appKey + cityGeo + userCity)// promise based
-    .then(function(response){
-      return response.json(); // parse the response data
-    })
-    .then(function(data) { // data should be an array or an object
-      console.log(data);
-      var image = data.avatar_url;
-      var repoUrl = data.repos_url;
-      // create
-      var imgEl = $('<img>');
-  
-      // text/attr
-      imgEl.attr({
-        alt: "avatar",
-        src: image
-      })
-  
-      // append
-      mainEl.append(imgEl);
-  
-      reposFetch(repoUrl);
-      
-    })
-  }
-  
-  function reposFetch(url){
-    fetch(url)// promise based
-    .then(function(response){
-      return response.json(); // parse the response data
-    })
-    .then(function(data) { // data should be an array or an object
-      console.log(data); // repo data
-  
-      for(var i = 0; i < data.length; i++){
-        console.log(data[i].name);
-  
-        var name = data[i].name;
-  
-        // create
-        var pEl = $('<p>');
-  
-        // text/attr
-        pEl.text(name);
-  
-        // append
-        mainEl.append(pEl);
-  
-      }
-    })
-  }
+var searchBtn = $('#searchBtn');
+var requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?'
+var appKey = 'a484d3fb0652e567788e79a914828975'//first parameter
+var lat = '&lat='
+var lon = '&lon='
+var geoUrl = 'http://api.openweathermap.org/geo/1.0/direct?'
+var cityGeo = '&q='
+var limit = '&limit=1'
 
-// fetch(requestUrl)
-// .then(function(response){
-//     return response.json();
-// })
-// .then(function(data){
-//     console.log(data);
-// })
-// .catch(function(error){
-//     console.error(error);
-// });
 
 var cityFormEl = $('#cityForm');//Form Element ID
 var previousCitiesEl = $('#previousCities');//UL ID
+var cityArray = JSON.parse(localStorage.getItem('cityArray')) || []
 
-function handleFormSubmit (event){
-    event.preventDefault();
-    
-    var value = $('input[name=cityName]').val();//variable created to hold the value entered into the input
 
-    firstFetch(value)//value is city
 
-    $('input[name=cityName]').val(''); // Resets the input field after you submit city entered in input   
-   
-        var cityBtn = $('<button>'); //create the button
+function handleFormSubmit(event) {
+  event.preventDefault();
+  var value = $('input[name=cityName]').val();//variable created to hold the value entered into the input
+  cityArray.push(value);
 
-        cityBtn.text(value); //add text to the button
-        
-        cityBtn.attr('class','prevCities'); // add a class to the button
-        
-        previousCitiesEl.append(cityBtn); //Append the button to the page
+  if (value == "") {
+    alert("Enter a city name to see the weather.");
+    return false;
+  }
 
-    localStorage.setItem('cityArray', value.value); //
 
-    
-    }
+  // firstFetch(value)//value is city
+  $('input[name=cityName]').val(''); // Resets the input field after you submit city entered in input   
+  var cityBtn = $('<button>');
+
+  cityBtn.text(value);
+  cityBtn.attr('class', 'prevCities');
+  cityBtn.on('click', function () {
+    getWeatherInfo(value);
+  })
+  previousCitiesEl.append(cityBtn);
+
+  localStorage.setItem('cityArray', JSON.stringify(cityArray)); //
+  console.log(cityArray);
+
+  getWeatherInfo(value);
+}
 
 cityFormEl.on('submit', handleFormSubmit);
+
+function getPrevCities() {
+  cityArray.forEach(function (currentValue) {
+    // console.log(currentValue)
+    var cityBtn = $('<button>');
+    cityBtn.text(currentValue);
+    cityBtn.attr('class', 'prevCities');
+    previousCitiesEl.append(cityBtn);
+    cityBtn.on('click', function () {
+      getWeatherInfo(currentValue);
+    })
+  });
+}
+
+getPrevCities()
+
+
+function getWeatherInfo(userCity) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userCity}&appid=${appKey}&units=imperial`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var temp = data.main.temp;
+      var windSpeed = data.wind.speed;
+      var humidity = data.main.humidity;
+      var cityName = data.name;
+      
+      $('#currentTemp').text(temp +' °F');
+      $('#currentWind').text(windSpeed +' MPH');
+      $('#currentHumidity').text(humidity +' %');
+      $('#cityName').text(cityName);
+      
+      console.log(data.name);
+      
+      getFiveDayWeather(data.coord.lat, data.coord.lon)
+
+    })
+
+}
+
+function getFiveDayWeather(lat, lon) {
+  fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${appKey}&units=imperial`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      // console.log(data);
+      for (var i = 0; i < data.list.length; i = i + 8) {
+        var temp = data.list[i].main.temp;
+        var windSpeed = data.list[i].wind.speed;
+        var humidity = data.list[i].main.humidity;
+
+        console.log(data.list[i])
+
+          var dayBlocks = $('#dayBlocks');
+          var weatherTile = $('<div>');
+              weatherTile.addClass('day');
+              weatherTile.text("Temp: " + temp +"°F");
+              dayBlocks.append(weatherTile);
+
+          // var tempLi = $('tempLi');
+          // tempLi.text('some text');
+          // tempLi.append(temp);
+      }
+    })
+}
